6a373638c6ad79259fcda743f9edf692faa2a74f
fengjun.chen <raychenfj@gmail.com>
2018-08-23 12:36:28 +0800

    add route, add return

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', deploymentRouter)
+app.use('/v1/deployment', deploymentRouter)
 
 module.exports = app
--- routes/deployment.js
+++ routes/deployment.js
@@ -7,28 +7,33 @@ const to = require('to-case')
 const exec = require('sync-exec')
 
 router.post('/', function (req, res, next) {
+  if (req.body) {
+    return res.status(500).send('empty request body')
+  }
   const repository = req.body.repository
   const repoConfig = config[repository.name]
   if (!repoConfig) {
-    res.status(500).send(`repository ${repository.name} is not registered`)
+    return res.status(500).send(`repository ${repository.name} is not registered`)
   }
 
   if (repoConfig.ref && repoConfig.ref !== req.body.ref) {
-    res.send('ref is not matched, skip deployment')
+    return res.send('ref is not matched, skip deployment')
   }
 
   const signature = res.get('X-Hub-Signature')
   const secret = process.env[`GITHUB_WEBHOOK_${to.snake(repository.name).toUpperCase()}_SECRET`]
 
   if (!secret) {
-    res.status.send(`can't find env variable GITHUB_WEBHOOK_${to.snake(repository.name).toUpperCase()}_SECRET`)
+    return res.status.send(`can't find env variable GITHUB_WEBHOOK_${to.snake(repository.name).toUpperCase()}_SECRET`)
   }
 
   if (signature && !util.verifySignature(signature, JSON.stringify(res.body), secret)) {
-    res.status(400).send('invalid signature')
+    return res.status(400).send('invalid signature')
   }
 
   repoConfig.scripts.forEach(cmd => console.info(exec(cmd)))
+
+  return res.send('deployment complete')
 })
 
 module.exports = router
