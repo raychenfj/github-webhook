224c0c55350871ff203002f3a95be3f4196af353
fengjun.chen <raychenfj@gmail.com>
2018-08-23 18:25:17 +0800

    update script

--- config.js
+++ config.js
@@ -4,13 +4,11 @@ module.exports = {
     events: ['push'],
     scripts: [
       'git clone https://github.com/raychenfj/psn-notification.git',
-      'cd psn-notification',
       'git checkout deployment',
       'docker rm -f psn-notification',
       'docker rmi psn-notification',
-      'docker build -t psn-notification .',
+      'docker build -t psn-notification ./psn-notification',
       'docker run -d --name psn-notification -e pass=$QQ_MAIL_PASS -p 3000:3000 psn-notification',
-      'cd ..',
       'rm -rf psn-notification'
     ]
   }
